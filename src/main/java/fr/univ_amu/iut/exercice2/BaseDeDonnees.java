b8715cab34c34a439edf7bfe52c6108c4b0a5c06
Mathias_M[25001942] <eabc2318@gmail.com>
2026-06-03 12:24:30 +0000

    Exercice 2 terminé

--- src/main/java/fr/univ_amu/iut/exercice2/BaseDeDonnees.java
+++ src/main/java/fr/univ_amu/iut/exercice2/BaseDeDonnees.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.sql.DataSource;
+import org.sqlite.SQLiteConfig;
+import org.sqlite.SQLiteDataSource;
 
 /**
  * Exercice 2 : fournir une {@link DataSource} SQLite et initialiser le schéma.
@@ -38,10 +40,15 @@ public class BaseDeDonnees {
     // TODO exercice 2 : créer et configurer la DataSource SQLite, et l'affecter à `source`.
     //
     // 1. SQLiteConfig config = new SQLiteConfig();
-    //    config.enforceForeignKeys(true);   // SQLite n'applique les FK que si on le demande
+    SQLiteConfig config = new SQLiteConfig();
+    config.enforceForeignKeys(true); // SQLite n'applique les FK que si on le demande
     // 2. SQLiteDataSource sqlite = new SQLiteDataSource(config);
+
     //    sqlite.setUrl("jdbc:sqlite:" + chemin);
+    SQLiteDataSource sqlite = new SQLiteDataSource(config);
+    sqlite.setUrl("jdbc:sqlite:" + chemin);
     // 3. source = sqlite;
+    source = sqlite;
 
     return source;
   }
